fa22257d0081d54d21dcb4968b4d8bd60036db2e
EgemenYapucu <56233612+EgemenYapucu@users.noreply.github.com>
2023-01-04 18:12:53 +0300

    Update TestCase1.java

--- TestCase1.java
+++ TestCase1.java
@@ -1,40 +1,39 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class TestCase1{
-// E-posta @ içermelidir.
+// Kullanıcı Adı yanlış girildiğinde.
 	public static void main(String[] args) {
 		// Kullanıcağımız driverı ve ulaşacağımız lokasyonu set ediyoruz
-			System.setProperty("webdriver.chrome.driver","D:\\Browser drivers\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver","D:\\Browser drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		    
-			String baseUrl = "http://localhost/";
-		    driver.get(baseUrl);
+		String baseUrl = "https://www.saucedemo.com/";
+		driver.get(baseUrl);
 		    
-		    //Sayfada bulunan her bir eleman için test dedğerlerini gönderiyoruz.
-		   	driver.findElement(By.name("E-Posta")).sendKeys("ardaovalıgmail.com");
-			driver.findElement(By.name("Şifre")).sendKeys("123456Ee");    
-			driver.findElement(By.name("Telefon Numarası")).sendKeys("05467891245");
-			driver.findElement(By.name("Bel Çevresi")).sendKeys("90");
-			driver.findElement(By.name("Omuz Çevresi")).sendKeys("60");
-			driver.findElement(By.name("Kilo")).sendKeys("90");
-			driver.findElement(By.name("Kaydet")).click();
+		//Sayfada bulunan her bir eleman için test dedğerlerini gönderiyoruz.
+	   	driver.findElement(By.id("user-name")).sendKeys("Egemen");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");    
+		driver.findElement(By.id("login-button")).click();
 			
-			String expected_title = ""; //Bir sonraki sayfanın başlığını bekliyoruz.
-			String actual_title = driver.getTitle();// Başlık alınır.
+		String expected_string = "Epic sadface: Username and password do not match any user in this service"; //Sayfanın error mesajı.
+		String actual_string = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]")).getText();// Metin alınır.
+
+		if(expected_string.equals(actual_string)){
+			System.out.println("Case 1 successful");
+		}else {
+			System.out.println("Case 1 failed");
+		}//Kayıt olursak test başarısız demektir.
 			
-			if(expected_title.equals(actual_title)){
-				System.out.println("Case 1 failed");
-			}else {
-				System.out.println("Case 1 successful");
-			}//Kayıt olursak test başarısız demektir.
-			
-		   	//Tarayıcıyı kapatıyoruz.
-		   	driver.close();
+	   	//Tarayıcıyı kapatıyoruz.
+	   	driver.close();
 	}
 
 }
+
+
+
+
 
 
 
